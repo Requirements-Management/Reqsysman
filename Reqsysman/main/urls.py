401714ed8fbffdd3c9f1a4580bce325de6ad8b2b
Personioto <outright.sm@gmail.com>
2023-05-17 15:39:10 +0700

    urls combined after merge

--- Reqsysman/main/urls.py
+++ Reqsysman/main/urls.py
@@ -11,20 +11,22 @@ router.register(r'requirements', RequirementViewSet)
 app_name = 'main'
 
 urlpatterns = [
+    # 1 V
     path('', views.index),
-
     path('admin/', admin.site.urls),
-    #path('add/', views.add_requirement, name='add_requirement'),
     path('Reqsysman/', views.requirements_list, name='requirements_list'),
-
-#    path('Reqsysman/add/', views.add_requirement, name='add_requirement'),
     path('', views.requirements_list, name='requirements_list'),
-    #path('Reqsysman/', views.Reqsysman),
-    #path('Reqsysman/', requirements_list, name='requirements_list'),
-    #path('Reqsysman/', include(router.urls)),
     path('github/', views.github),
 
+    # 2 I
     path('Reqsysman/', views.Reqsysman),
     path('Reqsysman/', include(router.urls)),
     path('github/', views.github)
+
+    # commented
+    #path('add/', views.add_requirement, name='add_requirement'),
+    #path('Reqsysman/add/', views.add_requirement, name='add_requirement'),
+    #path('Reqsysman/', views.Reqsysman),
+    #path('Reqsysman/', requirements_list, name='requirements_list'),
+    #path('Reqsysman/', include(router.urls)),
 ]
