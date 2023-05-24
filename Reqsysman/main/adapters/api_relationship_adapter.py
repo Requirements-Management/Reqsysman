9ea48d9e28e5e786056b8830f9f59ebeb3bbec11
Adefful <adefful46@yandex.ru>
2023-05-24 23:51:46 +0300

    Обнавлены адаптеры

--- Reqsysman/main/adapters/api_relationship_adapter.py
+++ Reqsysman/main/adapters/api_relationship_adapter.py
@@ -11,3 +11,15 @@ class ApiRelationShipsAdapter:
     def create_relationship(self, source_requirement: str, target_requirement: str, relationship_type: str, branch: str, commit_hash: str) -> dict:
         # Заглушка для создания связи между требованиями на определенной ветке и коммите через API
         pass
+
+    def update_relationship(self, relationship_id: str, source_requirement: str, target_requirement: str, relationship_type: str, branch: str, new_commit_hash: str) -> dict:
+        # Заглушка для обновления связи между требованиями через API
+        pass
+
+    def delete_relationship(self, relationship_id: str, branch: str, commit_hash: str) -> dict:
+        # Заглушка для удаления связи между требованиями через API
+        pass
+
+    def get_relationship(self, relationship_id: str, branch: str, commit_hash: str) -> dict:
+        # Заглушка для получения информации о конкретной связи между требованиями через API
+        pass
--- Reqsysman/main/adapters/api_requirements_adapter.py
+++ Reqsysman/main/adapters/api_requirements_adapter.py
@@ -4,17 +4,22 @@ class ApiRequirementsAdapter:
     def __init__(self):
         pass
 
-    def get_all_requirements(self, commit_id: str = None, branch: str = None) -> List[Dict[str, Union[str, int]]]:
+    def get_all_requirements(self, id: str = None, branch: str = None, commit_hash: str = None) -> List[Dict[str, Union[str, int]]]:
         # Здесь мы предполагаем, что API предоставляет endpoint для получения всех требований.
         # В реальной реализации здесь будет вызов к API с передачей commit_id и branch как параметров.
         pass
 
-    def get_requirement_by_id(self, id: str, commit_id: str = None, branch: str = None) -> Dict[str, Union[str, int]]:
+    def get_requirement_by_id(self, id: str, branch: str = None, commit_hash: str = None) -> Dict[str, Union[str, int]]:
         # Здесь мы предполагаем, что API предоставляет endpoint для получения требования по ID.
         # В реальной реализации здесь будет вызов к API с передачей commit_id и branch как параметров.
         pass
 
-    def update_requirement(self, id: str, data: Dict[str, Union[str, int]], commit_id: str = None, branch: str = None) -> Dict[str, Union[str, int]]:
+    def update_requirement(self, id: str, data: Dict[str, Union[str, int]], branch: str = None, commit_hash: str = None) -> Dict[str, Union[str, int]]:
         # Здесь мы предполагаем, что API предоставляет endpoint для обновления требования.
+        # В реальной реализации здесь будет вызов к API с передачей commit_id и branch как параметров.
+        pass
+
+    def delete_requirement(self, id: str, branch: str = None, commit_hash: str = None) -> Dict[str, str]:
+        # Здесь мы предполагаем, что API предоставляет endpoint для удаления требования.
         # В реальной реализации здесь будет вызов к API с передачей commit_id и branch как параметров.
         pass
